9bce4588295e77e9a6e324c60e0a5ded1cf4fe4e
0xbm <0xmb@protonmail.com>
2022-10-03 19:31:31 +0200

    code w/ soft edit

--- sender.py
+++ sender.py
@@ -1,1 +1,39 @@
+import smtplib
+import mimetypes
+from email.message import EmailMessage
+message = EmailMessage()
+sender = "asd@gmail.com"
+recipient ="asd@gmail.com"
+message['From'] = sender
+message['To'] = recipient
+message['Subject'] = 'Learning Python'
+body = "Hello"
+message.set_content(body)
+mime_type, _ = mimetypes.guess_type('filename')
+mime_type, mime_subtype = mime_type.split('/')
+
+with open('PATH', 'rb') as file:
+    message.add_attachment(file.read(),
+    maintype=mime_type,
+    subtype=mime_subtype,
+    filename='filename')
+print(message)
+
+
+smtp = smtplib.SMTP("smtp.gmail.com", 587)
+smtp.starttls()
+
+smtp.login("asd@gmail.com", 'asd')
+smtp.send_message(message)
+smtp.quit()
+
+
+
+
+
+
+
+
+
+
 
