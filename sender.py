aaee9a6eec627d673cc808abf7b1707537ce8eb3
0xbm <0xmb@protonmail.com>
2022-10-05 14:28:49 +0200

    add loop and rename function

--- sender.py
+++ sender.py
@@ -2,6 +2,35 @@ import smtplib
 import mimetypes
 from email.message import EmailMessage
 
+
+class Send:
+    def connect(self):
+        smtp = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp.starttls()
+        smtp.set_debuglevel(1)
+
+        smtp.login(sender_email, sender_passwd)
+        smtp.send_message(message)
+        smtp.quit()
+
+
+class Text(Send):
+    def push(self):
+        super().connect()
+
+
+class Attachment(Send):
+    def push_attachment(self):
+        file_path = input("Paste file PATH to send: ")
+        mime_type, _ = mimetypes.guess_type(file_path)
+        mime_type, mime_subtype = mime_type.split("/")
+        with open(file_path, "rb") as file:
+            message.add_attachment(
+                file.read(), maintype=mime_type, subtype=mime_subtype
+            )
+        super().connect()
+
+
 sender_email = input("Type your adress: ")
 recipient_email = input("Type recipient adress: ")
 sender_passwd = input("Type your password(your token from gmail account): ")
@@ -15,28 +44,20 @@ message["Subject"] = "No Subject"
 body = input("Your message: ")
 message.set_content(body)
 
-choose = input("1-text or enter-attachment?")
-if choose == "1":
-    smtp = smtplib.SMTP("smtp.gmail.com", 587)
-    smtp.starttls()
-    smtp.set_debuglevel(1)
 
-    smtp.login(sender_email, sender_passwd)
-    smtp.send_message(message)
-    smtp.quit()
-else:
-    file_path = input("Paste file PATH to send: ")
-    mime_type, _ = mimetypes.guess_type(file_path)
-    mime_type, mime_subtype = mime_type.split("/")
-    with open(file_path, "rb") as file:
-        message.add_attachment(file.read(), maintype=mime_type, subtype=mime_subtype)
-        # filename='filename')
-    # print(message)
+send = Send()
+text = Text()
+attachment = Attachment()
+while True:
+    choose = input("1-text or enter-attachment?")
 
-    smtp = smtplib.SMTP("smtp.gmail.com", 587)
-    smtp.starttls()
-    smtp.set_debuglevel(1)
+    if choose == "q":
+        break
 
-    smtp.login(sender_email, sender_passwd)
-    smtp.send_message(message)
-    smtp.quit()
+    elif choose == "1":
+        text.push()
+        print("Mail sended.")
+
+    else:
+        attachment.push_attachment()
+        print("Attachment sended.")
