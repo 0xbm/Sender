8302bd4a97e863964c67eb4dea2c1734495af645
0xbm <0xmb@protonmail.com>
2022-10-09 17:34:50 +0200

    add import instead execfile

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 while True:
-    choice = input("Would you like send 1.@mail, 2.SMS? or q for quit: ")
+    choice = input("Would you like send 1.EMAIL, 2.SMS? or q for quit: ")
     if choice == "q":
         break
     elif choice == "1":
-        exec(open("mail.py").read())
+        import mailSender
+        #exec(open("mailSender.py").read())
     elif choice == "2":
-        exec(open("sms.py").read())
+        import smsSender
+        # exec(open("smsSender.py").read())
     else:
         print("Choose between 1 or 2 or q: ")
