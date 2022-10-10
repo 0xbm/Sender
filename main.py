22cba1714e55a1b5ebc732690e849226ed8c839b
0xbm <0xmb@protonmail.com>
2022-10-10 19:18:58 +0200

    reverse from import to exec

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+choice = input("Would you like send 1.EMAIL, 2.SMS or q for quit: ")
+
 while True:
     choice = input("Would you like send 1.EMAIL, 2.SMS? or q for quit: ")
     if choice == "q":
         break
     elif choice == "1":
-        import mailSender
-        #exec(open("mailSender.py").read())
+        exec(open("mailSender.py").read())
     elif choice == "2":
-        import smsSender
-        # exec(open("smsSender.py").read())
+        exec(open("smsSender.py").read())
     else:
         print("Choose between 1 or 2 or q: ")
