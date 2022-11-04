3060b3e7584dd76e3370799897c8b2e0883b2abc
0xbm <0xmb@protonmail.com>
2022-11-04 22:01:59 +0100

    commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sms.smsSender import Sms
 from mail.mailSender import Mail
 
 
-def asd(self):
+def main(self):
     choice = input("Would you like send 1.EMAIL, 2.SMS? or q for quit: ")
     match choice:
         case "1":
@@ -11,7 +11,11 @@ def asd(self):
             Sms.push_sms(Sms)
         case "q":
             breakpoint
-            print("QUIT")
+            print("\x1b[5;30;42mQUIT")
 
 
-asd(asd)
+main(main)
+
+# if __name__ == "__main__":
+#     main()
+#     print("ASD")
